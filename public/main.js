0d24d02b675f639004f9014340238339916d9d8d
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-01-26 11:06:25 +0100

    trying netlifly

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const dirPath = path.join(__dirname, "../posts");
-const dirPathPages = path.join(__dirname, "../src/pages/content");
+const dirPathPages = path.join(__dirname, "../src/content");
 let postlist = [];
 let pagelist = [];
 
