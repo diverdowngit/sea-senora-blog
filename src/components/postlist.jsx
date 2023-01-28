6d07a68a1a448073082e06c96719f6d1cff69bff
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-01-28 14:46:08 +0100

    tryagain

--- src/components/postlist.jsx
+++ src/components/postlist.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link }  from "react-router-dom"
 import Markdown from "react-markdown"
-<<<<<<< HEAD
 import AllPosts from "../posts.json"
 import styles from  "./postlist.module.css"
 import BlogCard from "../modules/user/pages/Home/components/LatestBlog/components/BlogCard/BlogCard"
@@ -46,52 +45,11 @@ const PostList = (props) => {
                          
                         </div>
                         
-=======
-import postlist from "../posts.json"
-import "./postlist.css"
-
-
-const PostList = () => {
-    const excerptList = postlist.map(post => {
-        return post.content.split(" ").slice(0, 20).join(" ") + "..."
-    })
-    return (
-        <div className="postlist">
-            <h1 className="title">Blogs</h1>
-            {postlist.length && 
-                postlist.map((post, i) => {
-                    return (
-                        <div key={i} className="post-card">
-                             <div className="img-container">
-                                {post.thumbnail && <img className="thumbnail" width={500} src={post.thumbnail} alt={post.thumbnail}/> }
-                                <h2 className="post-title"><Link className="links" to={`/post/${post.id}`}>{post.title}</Link></h2>
-                            </div>
-                            <small>Published on {post.date} by {post.author}</small>
-                           
-                            <Markdown source={excerptList[i]} escapeHtml={false} />
-                            <small><Link className="links" to={`/post/${post.id}`}>Read more</Link></small>
-                            <hr/>
-                        </div>
->>>>>>> parent of 8920834 (trying netlifly)
                     )
                 })
             }
-        </div>
-<<<<<<< HEAD
-        </div>
-         </div> 
-        
-        
-        
-        
-        
-        
-        
-        
-        ) 
-=======
+        </div> </div> </div>
     )
->>>>>>> parent of 8920834 (trying netlifly)
 }
 
 export default PostList
