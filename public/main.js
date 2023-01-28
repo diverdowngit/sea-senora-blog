dbdcf73709ed27a817076f809756cfb54524b182
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-01-28 11:48:19 +0100

    trying netlifly

--- public/main.js
+++ public/main.js
@@ -78,7 +78,7 @@ const getPosts = () => {
         const metadataIndices = lines.reduce(getMetadataIndices, []);
         const metadata = parseMetadata({ lines, metadataIndices });
         const content = parseContent({ lines, metadataIndices });
-        const intro = parseContent({ lines, metadataIndices });
+       
         const parsedDate = metadata.date
           ? formatDate(metadata.date)
           : new Date();
